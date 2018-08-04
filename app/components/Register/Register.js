9e5535f75957d306bdfb6d1f9bafd0857b6f195c
Alex <alexkmartinez77@gmail.com>
2018-08-04 16:55:37 -0700

    First draft of Registration Form with input fields and a Register Button displays when Register in NavBar is clicked

--- app/components/Register/Register.js
+++ app/components/Register/Register.js
@@ -1,5 +1,65 @@
 import React from 'react';
+import { StyleSheet, Text, View, TextInput, Linking, Alert, ScrollView } from 'react-native';
+import { FormLabel, FormInput, Button, FormValidationMessage, Icon } from 'react-native-elements';
 
 export default class Register extends React.Component {
-    
+
+    render() {
+        return (
+            <ScrollView keyboardDismissMode='on-drag'>
+                <View style={styles.container}>
+                    <View style={styles.formContainer}>
+                        <FormLabel>First Name </FormLabel>
+                        <FormInput />
+                        <FormLabel>Last Name</FormLabel>
+                        <FormInput />
+                        <FormLabel>Email</FormLabel>
+                        <FormInput />
+                        <FormLabel>Phone Number</FormLabel>
+                        <FormInput />
+                        <FormLabel>Password</FormLabel>
+                        <FormInput />
+                        <FormLabel>Re-Type Password</FormLabel>
+                        <FormInput />
+                    </View>
+                    <View>
+                    </View>
+                    <Button style={styles.button}
+                        backgroundColor={'#346abb'}
+                        borderRadius={3}
+                        medium
+                        icon={{ name: 'sign-in', type: 'font-awesome' }}
+                        title='REGISTER' />
+                </View>
+            </ScrollView>
+        )
+    }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#DCDCDC',
+        alignItems: 'center',
+        paddingTop: 30,
+        paddingBottom: 300
+    },
+    button: {
+        marginTop: 55,
+        marginBottom: 20,
+        width: 320
+    },
+    socialButtonsContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        width: 350,
+        paddingTop: 8,
+        paddingHorizontal: 25
+    },
+    formContainer: {
+        width: 350
+    },
+    switchToLogin: {
+    }
+});
--- app/screens/RegisterScreen.js
+++ app/screens/RegisterScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ScrollView, StyleSheet } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
+import Register from '../components/Register';
 
 export default class RegisterScreen extends React.Component {
     static navigationOptions = {
@@ -10,9 +10,7 @@ export default class RegisterScreen extends React.Component {
     render() {
         return (
             <ScrollView style={styles.container}>
-                {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
-                <ExpoLinksView />
+                <Register />
             </ScrollView>
         );
     }
